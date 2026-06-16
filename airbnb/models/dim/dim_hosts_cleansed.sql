8c8a67a3710fd899cea45d4266fd72a6e2e29f4c
Christian Burbon <62530849+christianburbon@users.noreply.github.com>
2026-06-17 00:25:05 +0930

    Update dim_hosts_cleansed.sql
    updated select statement to include all columns to be more explicit in table contents

--- airbnb/models/dim/dim_hosts_cleansed.sql
+++ airbnb/models/dim/dim_hosts_cleansed.sql
@@ -2,6 +2,9 @@ WITH src_hosts AS (
     SELECT * FROM {{ref('src_hosts')}}
 )
 SELECT
-    * EXCLUDE (host_name)
+    host_id
     ,NVL(host_name,'Anonymous') as host_name
+    ,is_superhost
+    ,created_at
+    ,updated_at
 FROM src_hosts
